3b6014d6ead1c36799fc411db366f501d16d8849
wictus <szymon.niedzwiecki@googlemail.com>
2017-08-24 13:09:06 +0200

    Add reading data from files

--- dataFile.cpp
+++ dataFile.cpp
@@ -1,15 +1,20 @@
 #include "dataFile.h"
 
+
 dataFile::dataFile(const dataFile& file)
 : fPath(file.fPath)
 {
       
 }
 
-dataFile::dataFile(std::string path)
+dataFile::dataFile(const std::string& path)
 :fPath(path)
 {
-  
+  if(!checkPath())
+  {
+    std::cout<< "Bad path to file: " << path << std::endl;
+    std::exit(2);
+  }
 }
 
 dataFile::dataFile()
@@ -28,4 +33,28 @@ dataFile::~dataFile()
 
 }
 
+void dataFile::readData()
+{
+  std::ifstream file;
+  file.open(fPath.c_str());
+  std::string line;
+  while( std::getline(file, line) )
+  {
+    std::istringstream iss(line);
+    double bin, height;
+    if (! ( iss >> bin >> height ) )
+      break;
+    fData.push_back(std::make_pair<>(bin, height));
+  }
+  std::cout << "Read file with: " << fData.size() << " lines\n";
+  if(fData.size() == 0)
+  {
+    std::cout << "Problem with file: " << fPath << std::endl;
+    std::exit(2);
+  }
+}
 
+std::string dataFile::getPath()
+{
+  return fPath;
+}
--- dataFile.h
+++ dataFile.h
@@ -2,7 +2,14 @@
 
 #define DATAFILE
 
+#include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include <utility>
+#include <iterator>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -11,11 +18,14 @@ class dataFile
 {
 public:
   dataFile();
-  dataFile(std::string path);
+  dataFile(const std::string& path);
   ~dataFile();
   dataFile(const dataFile& );
   dataFile& operator= (dataFile&);
+  void readData();
+  std::string getPath();
 protected:
+  std::vector<std::pair<double,double> > fData;
   inline bool checkPath(){ struct stat buffer; return (stat ( fPath.c_str(), &buffer) == 0); };
   std::string fPath;
 };
