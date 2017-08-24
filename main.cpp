f091fc64b85ad94d67c7b9ea22fcfe0740a53e64
wictus <szymon.niedzwiecki@googlemail.com>
2017-08-24 13:36:07 +0200

    Main function extended with fitting example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "dataFile.h"
+#include "elementSpectrum.h"
+#include "spectrumFitter.h"
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+  
+  std::string filePath = "";
+  std::string outPath = "";
+  if(3 == argc)
+  {
+    filePath = argv[1];
+    outPath = argv[2];
+  }
+  
+  elementSpectrum file(argv[1]);
+  file.readData();
+  
+  elementSpectrum file2(argv[2]);
+  file2.readData();
+  
+  spectrumFitter test(file, file2);
+  test.fit(argv[2]);
+  
+  return 0;
+}
+
+void exampleOfElementSpectrumUsage(const std::string& filePath, const std::string& outPath)
+{
+  elementSpectrum file(filePath);
+   file.readData();
+   file.plot(outPath);
+    
 }
