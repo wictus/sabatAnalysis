f361ef87a772e4acd967d7f5a56389864071815b
wictus <szymon.niedzwiecki@googlemail.com>
2017-09-04 12:12:56 +0200

    Fix bugs withing spectrum fitter

--- spectrumFitter.cpp
+++ spectrumFitter.cpp
@@ -20,15 +20,15 @@ spectrumFitter::spectrumFitter(const elementSpectrum& firstSpectrum, const eleme
 
 void spectrumFitter::fit(std::string out)
 {
-  globalHisto = fFirstSpectrum.getHisto();
+  globalHisto = &fFirstSpectrum.getHisto();
   fFnc = new TF1("fFnc", simulateFunction, 0, 11, 1);
   fFnc->SetParameters(0, globalHisto->GetMaximum() );
   fFnc->SetParLimits(0,0, 10000);
   fFnc->SetNumberFitPoints(100000);
   
   TCanvas* c = new TCanvas();
-  fSecondSpectrum.getHisto()->Draw();
-  fSecondSpectrum.getHisto()->Fit("fFnc", "M");
+  fSecondSpectrum.getHisto().Draw();
+  fSecondSpectrum.getHisto().Fit("fFnc", "M");
   std::cout << "Chi square: " << fFnc->GetChisquare() << std::endl;
   c->SetLogy(1);
   TString outName = out;
