acadf78b42c685bbb07ea9f25e9239bb368e097f
Szymon <wictusn@gmail.com>
2019-04-01 13:25:59 +0200

    change fiting parameters and limits

--- spectrumFitter.cpp
+++ spectrumFitter.cpp
@@ -1,4 +1,5 @@
 #include "spectrumFitter.h"
+#include <boost/iterator/iterator_concepts.hpp>
 
 std::vector<TH1F*> globalHisto;
 
@@ -41,16 +42,17 @@ void spectrumFitter::fit(std::string out)
     int length = name.find_last_of("/") - (name.substr(0, name.find_last_of("/"))).find_last_of("/");
     name= name.substr( (name.substr(0, name.find_last_of("/"))).find_last_of("/")+1 , length-1);
     fFnc->SetParName(i,name.c_str());
-    fFnc->SetParLimits(i,0, 20 );
+//     fFnc->SetParLimits(i,0, 100 );
   }
  
   fFnc->SetNumberFitPoints(100000);
   
   TH1F* finalHisto = new TH1F("final", "final", fBins, fStart, fStop);
   TCanvas* c = new TCanvas();
+  c->SetWindowSize(500,500);
   fPrimarySpectrum.getHisto().Draw("hist");
   if( fTypeOfFit == chi2 )
-    fPrimarySpectrum.getHisto().Fit("fFnc", "0EMR");
+    fPrimarySpectrum.getHisto().Fit("fFnc", "0EMRs");
   else if ( fTypeOfFit == WL )
     fPrimarySpectrum.getHisto().Fit("fFnc", "0EMRWL");
   else 
@@ -60,34 +62,57 @@ void spectrumFitter::fit(std::string out)
   }
   
   std::cout << "Chi square: " << fFnc->GetChisquare()/fFnc->GetNDF() << std::endl;
-    
+  
+
   for( unsigned int i = 0; i < fSpectraToFit.size(); i++)
   {
     fSpectraToFit[i].getHisto().Scale( fFnc->GetParameter(i));
     fSpectraToFit[i].getHisto().SetLineColor(2+i);
+    fSpectraToFit[i].getHisto().SetLineWidth(2);
     fSpectraToFit[i].getHisto().Draw("histsame");
     finalHisto->Add( &fSpectraToFit[i].getHisto());
   }
-   
+  
+  finalHisto->SetLineColor(1);
+  finalHisto->SetLineWidth(2);
   TLegend legend = buildLegend();
   legend.Draw();
   c->SetLogy();
 
   TString outName = out;
-  outName+=".png";
-  c->SaveAs(outName);
+  c->SaveAs(outName+".png");
+  c->SaveAs(outName+".root");
+  
   finalHisto->SetLineColor(1);
   finalHisto->SetLineWidth(2);
   finalHisto->Draw("hist");
   finalHisto->SetMinimum(1e-8);
   fPrimarySpectrum.getHisto().Draw("histsame");
   c->SaveAs("fit.png");
+  c->SaveAs("fit.root");
   
-  fPrimarySpectrum.getHisto().Add(finalHisto, -1);
+  
+  
+
+  for( unsigned int i = 0; i < fSpectraToFit.size(); i++)
+  {
+    fPrimarySpectrum.getHisto().Draw("hist");
+    fSpectraToFit[i].getHisto().Draw("histsame");
+    TLegend legend = buildLegend();
+  legend.Draw();
+  c->SetLogy();
+    TString name;
+    name += fFnc->GetParName(i);
+    c->SaveAs(name+".png");
+    c->SaveAs(name+".root");
+  }
+  
+    fPrimarySpectrum.getHisto().Add(finalHisto, -1);
   fPrimarySpectrum.getHisto().Draw("hist");
   fPrimarySpectrum.getHisto().SetTitle("Experimental - fit");
   c->SaveAs("difference.png");
-  
+  c->SaveAs("difference.root");
+    
   delete c;
   delete finalHisto;
 }
